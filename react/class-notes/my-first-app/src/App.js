d5cfa73113bc66645b5059db648e79198b183307
1yavuz <1yavuzahmet@gmail.com>
2024-12-24 23:17:42 +0300

    component created

--- react/class-notes/my-first-app/src/App.js
+++ react/class-notes/my-first-app/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import helloReact from "./assets/react.png"
+import Header from './components/header';
+
+
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Header></Header>
       <img src={helloReact} alt="img" className="picture" />
     </div>
   );
---
+++ react/class-notes/my-first-app/src/components/Header.jsx
@@ -0,0 +1,8 @@
+function Header() {
+    return (
+
+        <h1>My first React Project</h1>
+    );
+}
+
+export default Header;
