7636e36e5a2f3f21fe76a1a969f73028b232aa20
1yavuz <1yavuzahmet@gmail.com>
2024-12-26 01:05:33 +0300

    patika react component lesson completed

---
+++ external-source/patika/components-lesson/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Header';
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello </h1>
+      <hr></hr>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
