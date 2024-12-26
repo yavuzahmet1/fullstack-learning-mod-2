763d0956ab6ea0c8c3989b8cba48ef596e2ef638
1yavuz <1yavuzahmet@gmail.com>
2024-12-26 23:30:01 +0300

    project completed with scss style

---
+++ react/class-notes/rc-03-styling/styling-sass/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import "../scss/footer.scss";
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>Copyright by FS-18 - {new Date().getFullYear()} </p>
+        </footer>
+    );
+};
+export default Footer;
