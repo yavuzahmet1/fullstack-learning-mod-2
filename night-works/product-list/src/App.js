6f33ddbc35dd685cf425f4701c0beff8d505c628
1yavuz <1yavuzahmet@gmail.com>
2024-12-28 21:03:31 +0300

    night work structure created

---
+++ night-works/product-list/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Header from './components/Header/Header';
+import ProductCard from './components/ProductCard/ProductCard';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <ProductCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ night-works/product-list/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProductCard from '../ProductCard/roductCard'
+
+function Header() {
+    return (
+        <div>Header
+            <ProductCard />
+        </div>
+    )
+}
+
+export default Header
