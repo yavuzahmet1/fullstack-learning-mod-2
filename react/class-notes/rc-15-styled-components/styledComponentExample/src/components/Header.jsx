4cb1c87155d70e23e7c74494c9a71a0b769c394a
1yavuz <1yavuzahmet@gmail.com>
2025-01-16 21:28:44 +0300

    header content created

--- react/class-notes/rc-15-styled-components/styledComponentExample/src/components/Header.jsx
+++ react/class-notes/rc-15-styled-components/styledComponentExample/src/components/Header.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      <div>
+        <div>
+          <img src="./images/logo.png" width={200} alt="clarusway logo" />
+        </div>
+        <div>
+          <button>Apply Courses</button>
+          <button>Talk to Adviser</button>
+        </div>
+
+        <div>
+          <h1>The IT Career of Your Dreams Starts Here</h1>
+          <p>9:24
+            Clarusway is a leading international software Bootcamp. Join a micro
+            class online with other trainees and learn coding skills with a
+            highly-skilled instructor.</p>
+          <button>Start Your New Career</button>
+          <button>Details</button>
+        </div>
+        <img src="./images/hero.jpg
+        " alt="" />
+      </div>
+    </div>
   )
 }
 
