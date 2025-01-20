82025682b7a7b2ccaf68c002a73bad9ad1962c67
1yavuz <1yavuzahmet@gmail.com>
2025-01-20 20:31:55 +0300

    getData with axios

--- react/class-notes/rc-16-recipe-app-project/src/pages/home/RecipeCard.jsx
+++ react/class-notes/rc-16-recipe-app-project/src/pages/home/RecipeCard.jsx
@@ -1,13 +1,30 @@
 import React, { useContext } from 'react'
 import { RecipeContext } from '../../context/RecipeProvider'
+import { Button, Cards, MainContainer, RecipeHeader, RecipeImage } from "./HomeStyles"
+import { useNavigate } from "react-router-dom";
 
 const RecipeCard = () => {
 
-  const{yemekler}=useContext(RecipeContext)
+  const { yemekler } = useContext(RecipeContext)
   console.log(yemekler);
-  
+
+  const navigate = useNavigate()
+
   return (
-    <div>RecipeCard</div>
+    <MainContainer>
+      {
+        yemekler.map(({ recipe }) => (
+
+          <Cards key={recipe.calories}>
+            <RecipeHeader>
+              {recipe.label}
+            </RecipeHeader>
+            <RecipeImage src={recipe.image} />
+            <Button onClick={() => navigate("/details", { state: { recipe } })}>Details</Button>
+          </Cards>
+        ))
+      }
+    </MainContainer>
   )
 }
 
--- react/class-notes/rc-16-recipe-app-project/src/router/AppRouter.jsx
+++ react/class-notes/rc-16-recipe-app-project/src/router/AppRouter.jsx
@@ -4,18 +4,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "../pages/login/Login";
 import Home from "../pages/home/Home";
 import About from "../pages/about/About";
+import Details from '../pages/details/Details'
+
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<Login/>}/>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-       
+        <Route exact path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/details" element={<Details />} />
+
       </Routes>
 
-    
+
     </BrowserRouter>
   );
 };
