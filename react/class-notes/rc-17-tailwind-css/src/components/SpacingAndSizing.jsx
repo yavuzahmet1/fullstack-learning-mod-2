5e074ebec5a2f5b3d09bc0f505a95485b26a9583
1yavuz <1yavuzahmet@gmail.com>
2025-01-22 21:20:09 +0300

    button

--- react/class-notes/rc-17-tailwind-css/src/components/SpacingAndSizing.jsx
+++ react/class-notes/rc-17-tailwind-css/src/components/SpacingAndSizing.jsx
@@ -7,6 +7,11 @@ const SpacingAndSizing = () => {
             {/* //px-> sağden soldan padding
             //py->alt üst padding */}
             <button className='bg-blue-200 text-white rounded-xl px-3 m-3 '>Click</button>
+            <button className=" border-green-800 border-[3px] rounded-md bg-green-300 p-3" >Click</button>
+            <div>
+                <button className='bg-amber-500 w-20 rounded-full m-5 p-2 text-blue-800'>Save</button>
+
+            </div>
         </div>
     )
 }
