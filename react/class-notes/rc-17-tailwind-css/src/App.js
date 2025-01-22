f99154041130b62c11933b324c8dc3016741857c
1yavuz <1yavuzahmet@gmail.com>
2025-01-22 20:43:53 +0300

    text properties

---
+++ react/class-notes/rc-17-tailwind-css/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Typography from './components/Typography';
+
+function App() {
+  return (
+    <div className="App">
+      <Typography />
+    </div>
+  );
+}
+
+export default App;
---
+++ react/class-notes/rc-17-tailwind-css/src/components/Typography.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Typography = () => {
+    return (
+        <div >
+            <p className='text-sm'>Typograph</p>
+            <p className='text-xl'>Typograph</p>
+            <p className='text-lg'>Typograph</p>
+            <p className='text-3xl'>Typograph</p>
+            <p className='text-[5rem]'>Typograph</p>
+        </div>
+    )
+}
+
+export default Typography
