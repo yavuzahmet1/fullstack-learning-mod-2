6f849df4da35ea75e5425b7ac3da41e56e369bce
1yavuz <1yavuzahmet@gmail.com>
2025-02-01 20:01:44 +0300

    re-work redux tolkit

---
+++ react/class-notes/rc-22-redux-toolkit-example-re-work/src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+
+
+const Navbar = () => {
+
+
+
+
+
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" color="secondary">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Clarusway News
+          </Typography>
+
+          (
+          <Button onClick={ } color="inherit">Logout</Button>
+          ) : (
+          <Button color="inherit">signup</Button>
+          )        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+
+}
+export default Navbar
