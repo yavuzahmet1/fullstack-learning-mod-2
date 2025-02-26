43e26f61ef11580e6c8a81a38a31c3e02d955d24
1yavuz <1yavuzahmet@gmail.com>
2025-02-26 20:04:22 +0300

    next js created

---
+++ nextjs/class-notes/netflix-app/src/app/page.js
@@ -0,0 +1,12 @@
+
+
+
+export default function Home() {
+  return (
+    <div className="relative h-screen w-full  ">
+      <div className="bg-black w-full h-full bg-opacity-50">
+       Home
+      </div>
+    </div>
+  );
+}
