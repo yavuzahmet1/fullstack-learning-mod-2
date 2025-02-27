5afc9fdb13126228f3b242e64773f41ce283902f
1yavuz <1yavuzahmet@gmail.com>
2025-02-27 23:51:37 +0300

    nextjs movie app completed

--- nextjs/class-notes/movie-app/src/helpers/movieFunction.js
+++ nextjs/class-notes/movie-app/src/helpers/movieFunction.js
@@ -7,7 +7,7 @@ export const getirMovies=async(type)=>{
 
   //? next.js ile fetch api çekilen verileri default olarak cache'ler. bu özelliği option objesi ile değiştirebiliriz
   //   const res = await fetch(URL, { cache: "force-cache" }); default
-  
+
   //   const res = await fetch(URL, { cache: "no-store" });
   //cache'leme
   //   const res = await fetch(URL, { next: { revalidate: 10 } });
@@ -22,3 +22,33 @@ export const getirMovies=async(type)=>{
   return results;
 }
 
+
+export const getirMovieDetail = async (movieId) => {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}`
+  );
+
+ 
+
+  if (!res.ok) {
+    throw new Error("fetch işleminde hata var");
+  }
+
+  const  data  = await res.json();
+
+  return data;
+};
+
+export const getirYoutubeKey = async (movieId) => {
+  const res = await fetch(
+   `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${API_KEY}`
+  );
+
+  if (!res.ok) {
+    throw new Error("fetch işleminde hata var");
+  }
+
+  const {results} = await res.json();
+
+  return results[0]?.key
+};
